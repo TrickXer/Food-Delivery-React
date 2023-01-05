5af2d7c95e979a0dbeb8a4395316af56c8dde1c2
TrickXer <kingrabby10@gmail.com>
2023-01-05 14:22:36 +0530

    Updated

--- src/db/menuList.js
+++ src/db/menuList.js
@@ -3,56 +3,77 @@ export const items = [
         id: 1,
         title: 'Idli',
         rating: '',
-        price: '20.00',
+        price: '10.00',
         category: 'Breakfast, Dinner'
     },
     {
         id: 2,
         title: 'Dosa',
         rating: '',
-        price: '10.00',
+        price: '25.00',
         category: 'Breakfast, Dinner'
     },
     {
         id: 3,
         title: 'Pongal',
         rating: '',
-        price: '10.00',
+        price: '20.00',
         category: 'Breakfast'
     },
     {
         id: 4,
         title: 'Upma',
         rating: '',
-        price: '10.00',
+        price: '20.00',
         category: 'Breakfast, Dinner'
     },
     {
         id: 5,
-        title: 'Briyani',
+        title: 'Meals',
         rating: '',
-        price: '10.00',
+        price: '110.00',
         category: 'Lunch'
     },
     {
         id: 6,
-        title: 'Fried Rice',
+        title: 'Chicken Briyani',
         rating: '',
-        price: '10.00',
+        price: '100.00',
         category: 'Lunch'
     },
     {
         id: 7,
-        title: 'Chapati',
+        title: 'Mutton Briyani',
         rating: '',
-        price: '10.00',
-        category: 'Breakfast, Dinner'
+        price: '150.00',
+        category: 'Lunch'
     },
     {
         id: 8,
+        title: 'Chicken Fried Rice',
+        rating: '',
+        price: '95.00',
+        category: 'Lunch'
+    },
+    {
+        id: 9,
+        title: 'Veg Fried Rice',
+        rating: '',
+        price: '70.00',
+        category: 'Lunch'
+    },
+    {
+        id: 10,
+        title: 'Chapati',
+        rating: '',
+        price: '15.00',
+        category: 'Breakfast, Dinner'
+    },
+    {
+        id: 11,
         title: 'Naan',
         rating: '',
-        price: '10.00',
+        price: '22.00',
         category: 'Dinner'
     }
 ]
